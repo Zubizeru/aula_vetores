6805587bcc105658afca8f2aa4b05e070ace8bdf
José Elias Herminio Mendes <jose.ehmendes@senacsp.edu.br>
2024-09-04 18:38:16 -0300

    Atualizando o exercicio_1.js

--- exercicio_1.js
+++ exercicio_1.js
@@ -1,7 +1,7 @@
     // Crie um algoritmo que leia um vetor de 5 números inteiros e mostre-os.
     let num = [];
     let mensagem = "";
-    
+
     // Leitura dos números nos espaços
     for (let i = 0; i < 5; i++) {              // Esse loop define quantos números vão ser pedidos ao usuario
         num.push(prompt("Informe um número:"));// Os números são pedidos e puxados ao array num
@@ -15,25 +15,8 @@
             mensagem += `${num[i]}, `;// Adiciona vírgula e espaço
         }
     }
-
     alert(mensagem);
 
 
 
 
-/* Outra forma de fazer esse exercicio é com join()
-
-     function executarExercicio1() {
-    // Crie um algoritmo que leia um vetor de 5 números inteiros e mostre-os.
-    let num = [];
-
-    // Leitura dos números nos espaços
-    for (let i = 0; i < 5; i++) {
-        num.push(prompt("Informe um número:"));
-    }
-
-    // Uso do .join para ir adicionando as iterações a string mensagem
-    let mensagem = num.join(", ") + ".";
-
-    alert(mensagem);
-} */
